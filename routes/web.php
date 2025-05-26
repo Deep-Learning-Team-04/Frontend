430e5b8161edb9d817885b6c97349c3c1231a27f
Kadek Wiradnyana <revanaditya310@gmail.com>
2025-05-26 23:42:54 +0800

    First commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('landingpage');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+require __DIR__.'/auth.php';
