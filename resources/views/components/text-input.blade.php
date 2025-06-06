4a0fafdeb167a90aec1c415bd90d68c164854c08
Kadek Wiradnyana <revanaditya310@gmail.com>
2025-06-06 14:17:10 +0800

    perbaiki komponen text input

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,9 +1,18 @@
 @props(['disabled' => false])
 
-<input 
-    @disabled($disabled) 
-    {{ $attributes->merge([
-        'class' => 'border-gray-300 focus:border-[#7B9CD9] focus:ring-[#7B9CD9] rounded-md shadow-sm placeholder:text-gray-400'
-    ]) }} 
-/>
+@php
+    $baseClass = 'font-inter border-gray-300 rounded-md shadow-sm';
 
+    // Fokus hanya aktif saat tidak disabled, tanpa bg
+    $focusClass = $disabled ? '' : 'focus:border-[#7B9CD9] focus:ring-[#7B9CD9]';
+
+    // Background dan warna teks sesuai status disabled
+    $disabledClass = $disabled
+        ? 'bg-disabled text-[#ADB5AF]' //bg dan text dissabled
+        : 'bg-white text-neu900'; //bg dan text true
+@endphp
+
+<input
+    @disabled($disabled)
+    {{ $attributes->merge(['class' => "$baseClass $focusClass $disabledClass"]) }}
+/>
