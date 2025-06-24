63857d4227f6444d63a8492cc564d9df417cfdfb
Kadek Wiradnyana <revanaditya310@gmail.com>
2025-06-25 02:58:54 +0800

    Hapus file view admin dan komponen

--- resources/views/components/danger-button.blade.php
+++
@@ -1,6 +0,0 @@
-<button {{ $attributes->merge([
-    'type' => 'button',
-    'class' => 'bg-[#FFF1F1] text-[#F83B3E] text-[18px] font-medium font-inter rounded px-3 py-1 hover:bg-[#ffe1e2] transition'
-]) }}>
-    {{ $slot }}
-</button>
